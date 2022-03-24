6469bf8ec842b57d43e56b90d726704c912b5126
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2022-03-24 09:53:04 +0900

    serviceとrepositoryの作成

--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,42 +1,44 @@
-//package com.example.demo.service;
-//
-//import java.util.HashSet;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import com.example.demo.model.SiteUser;
-//import com.example.demo.repository.SiteUserRepository;
-//
-//@Service
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//
-//    private final SiteUserRepository userRepository;
-//    
-//    public UserDetailsServiceImpl(SiteUserRepository userRepository) {
-//    	this.userRepository = userRepository;
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username)
-//            throws UsernameNotFoundException {
-//
-//        var user = userRepository.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException(username + " not found");
-//        }
-//        return createUserDetails(user);
-//    }
-//
-//    public User createUserDetails(SiteUser user) {
-//        var grantedAuthorities = new HashSet<GrantedAuthority>();
-//        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
-//
-//        return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
-//    }
-//}
+package com.example.demo.service;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.SiteUser;
+import com.example.demo.repository.SiteUserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final SiteUserRepository userRepository;
+    
+    public UserDetailsServiceImpl(SiteUserRepository userRepository) {
+    	this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        SiteUser user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException(username + " not found");
+        }
+        return createUserDetails(user);
+    }
+
+    public User createUserDetails(SiteUser user) {
+    	//HashSetは、要素の重複を許可しない集合構造を表します。
+    	//追加した順に取得できるとは限らない
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+
+        return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
